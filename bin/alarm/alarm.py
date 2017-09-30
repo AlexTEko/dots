33b77bf785489a6ec92eb208ca93316afd6f8d6d
Dakota Walsh <kotawalsh@gmail.com>
2017-09-30 02:29:21 -0400

    add git ignore, and alarm script

---
+++ bin/alarm/alarm.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Alarm
+
+========================================
+oooo    oooo               .
+`888   .8P'              .o8
+ 888  d8'     .ooooo.  .o888oo  .oooo.
+ 88888[      d88' `88b   888   `P  )88b
+ 888`88b.    888   888   888    .oP"888
+ 888  `88b.  888   888   888 . d8(  888
+o888o  o888o `Y8bod8P'   "888" `Y888""8o
+               @nilsu.org
+=== Copyright (C) 2017  Dakota Walsh ===
+"""
+import os
+import sys
+import subprocess
+import argparse
+
+def getArgs():
+    # get the passed arguments
+    description = "alarm"
+    arg = argparse.ArgumentParser(description=description)
+
+    arg.add_argument("-t",
+            help="Amount of time for alarm")
+
+    return arg.parse_args()
+
+def main():
+    # main function for alarm
+
+    # collect arguments
+    arguments = getArgs()
+
+    # check for time and set it to a variable
+    if arguments.t:
+        time = arguments.t
+    else:
+        sys.exit("A time must be passed in with t")
+
+    # run sleep for the amount of time needed
+    print("alarm set for " + time)
+    subprocess.run(["sleep", time])
+
+    # play music
+    print("AWAKEN FOUL BEAST")
+    subprocess.run(["cvlc", "-R", "alarm.opus"])
+
+if __name__ == '__main__':
+    main()
